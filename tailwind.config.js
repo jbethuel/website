c93e7774a0c501b40828d1e632a2baec584f846d
JBethuel <bethuel1995@gmail.com>
2020-05-30 03:37:17 +0800

    fix?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './components/**/*.tsx',
+      './pages/**/*.tsx'
+    ],
+    defaultExtractor: content => content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+  },
   theme: {
     extend: {},
   },
