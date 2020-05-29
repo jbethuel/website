ec496c942d8320e9042f81db4ac25402ae832c02
JBethuel <bethuel1995@gmail.com>
2020-05-30 03:43:58 +0800

    fix?

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+import tailwindcss from 'tailwindcss'
+import autofixer from 'autoprefixer'
+import purge from '@fullhuman/postcss-purgecss'
+
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env'],
+  // plugins: ['tailwindcss', 'postcss-preset-env'],
+  plugins: [
+    tailwindcss,
+    autofixer,
+    purge({
+      content: [
+        './components/**/*.tsx',
+        './pages/**/*.tsx'
+      ],
+      defaultExtractor: content => content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    })
+  ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './components/**/*.tsx',
-      './pages/**/*.tsx'
-    ],
-    defaultExtractor: content => content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    // enabled: process.env.NODE_ENV === 'production',
+    // content: [
+    //   './components/**/*.tsx',
+    //   './pages/**/*.tsx'
+    // ],
+    // defaultExtractor: content => content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
   },
   theme: {
     extend: {},
