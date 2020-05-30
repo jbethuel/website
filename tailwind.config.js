7dd91f7ea888bf3e2a5182d93178ed032f80c17e
JBethuel <bethuel1995@gmail.com>
2020-05-30 10:27:04 +0800

    purgecss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './client/**/*.tsx',
+      './pages/**/*.tsx'
+    ],
+  },
   theme: {
     extend: {},
   },
