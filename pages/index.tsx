c9ba2ac993a331d6f754cfb3ae8a8399efa42b54
jbethuel <jbethueldc@gmail.com>
2020-08-15 22:29:58 +0800

    💬 update message

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,16 +11,21 @@ const Home = () => {
       <Navbar />
       <Container>
         <p className='text-white px-6'>
-          Hello! I'm Joseph Bethuel Dela Cruz, <br />
-          Software engineer based at Cagayan de Oro City, Philippines.
+          Hello! I'm Joseph Bethuel Dela Cruz, <br />a software engineer based
+          at Cagayan de Oro City, Philippines.
         </p>
         <p className='text-white px-6 mt-4'>
-          I have been a software engineer for more than 3 years in a startup
-          company which was founded locally and the said company is currently
-          operating in 6 cities here in the Philippines. It is my first job and
-          I am designated as a mobile software engineer using React Native. Even
-          though I am working on a mobile app, I have also worked on its backend
-          features.
+          I have been a software engineer for more than three (3) years. My
+          first job was with a startup company which was founded locally and it
+          is currently operating in several cities here in the Philippines. I
+          have been designated as a mobile software engineer using React Native
+          therein. Even though I am working on a mobile app, I have also worked
+          on numerous backend features.
+        </p>
+        <p className='text-white px-6 mt-4'>
+          Coding is more than just a job, it is one thing I am passionate about.
+          It excites me and it challenges me at the same time. I do my best to
+          gradually learn and discover new things about my craft.
         </p>
       </Container>
     </>
