dd7756900dc47e19237fc024f25f3e2c3211a228
jbethuel <jbethueldc@gmail.com>
2020-11-20 06:38:09 +0800

    lul

--- pages/gear.tsx
+++ pages/gear.tsx
@@ -10,18 +10,10 @@ const Gear = () => {
       <main className="gear">
         <h3 className="title">Macbook Pro 2020 13"</h3>
         <div className="specs">
-          <p>- i5 8th Gen</p>
-          <p>- Iris 645</p>
-          <p>- 8GB RAM DDR3</p>
+          <p>- Intel i5</p>
+          <p>- Intel Iris 645</p>
+          <p>- 8GB RAM</p>
           <p>- 256 SSD</p>
-        </div>
-
-        <h3 className="title">Custom PC</h3>
-        <div className="specs">
-          <p>- Ryzen 5 2600</p>
-          <p>- Asus TUF GTX 1660 Super</p>
-          <p>- Corsair Vengeance LPX 16GB 3200hz</p>
-          <p>- Kingston A2000 NVMe 256GB SSD</p>
         </div>
       </main>
     </div>
