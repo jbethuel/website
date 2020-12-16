8a22f247c96af2247860a92e4c628e4545912b2f
jbethuel <jbethueldc@gmail.com>
2020-12-16 21:07:02 +0800

    💄 icons and meta tags

--- components/head.tsx
+++ components/head.tsx
@@ -3,49 +3,71 @@ import Head from 'next/head'
 interface Props {
   title?: string
   description?: string
+  subject?: string
 }
 
 // todo tags
 // https://htmlhead.dev/
 const Header = ({
   title = 'JBethuel - Software Engineer',
-  description = 'JBethuel - Software Engineer'
+  description = 'JBethuel - Software Engineer',
+  subject = 'JBethuel - Software Engineer'
 }: Props) => (
   <Head>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content={description} />
+    <meta name="subject" content={subject} />
 
-    {/* facebook */}
-    <meta property="fb:app_id" content="123456789" />
-    <meta property="og:url" content="https://example.com/page.html" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Content Title" />
-    <meta property="og:image" content="https://example.com/image.jpg" />
-    <meta
-      property="og:image:alt"
-      content="A description of what is in the image (not a caption)"
-    />
-    <meta property="og:description" content="Description Here" />
-    <meta property="og:site_name" content="Site Name" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="article:author" content="" />
+    {/* Control the behavior of search engine crawling and indexing */}
+    <meta name="robots" content="index,follow" />
+    <meta name="googlebot" content="index,follow" />
 
-    {/* twitter */}
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@site_account" />
-    <meta name="twitter:creator" content="@individual_account" />
-    <meta name="twitter:url" content="https://example.com/page.html" />
-    <meta name="twitter:title" content="Content Title" />
-    <meta
-      name="twitter:description"
-      content="Content description less than 200 characters"
+    {/* Tells Google not to provide a translation for this document  */}
+    <meta name="google" content="notranslate" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+    <link
+      rel="apple-touch-icon"
+      sizes="114x114"
+      href="/apple-icon-114x114.png"
     />
-    <meta name="twitter:image" content="https://example.com/image.jpg" />
-    <meta
-      name="twitter:image:alt"
-      content="A text description of the image conveying the essential nature of an image to users who are visually impaired. Maximum 420 characters."
+    <link
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="/apple-icon-120x120.png"
     />
+    <link
+      rel="apple-touch-icon"
+      sizes="144x144"
+      href="/apple-icon-144x144.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="/apple-icon-152x152.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/apple-icon-180x180.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="192x192"
+      href="/android-icon-192x192.png"
+    />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+    <meta name="theme-color" content="#ffffff" />
 
     <title>{title}</title>
   </Head>
